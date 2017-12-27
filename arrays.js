d1dc5e4db239498d7a7ab07aab99d7fc545d4331
MAHAK RAJVANSHI <mahakrajvanshi@yahoo.com>
2017-12-27 22:07:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,15 @@
 var chocolateBars = ['snickers', 'hundred', 'grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, element){
+
+  var newArr = arr
+
+  newArr.unshift(element)
+
+  return newArr
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element){
+
+  return arr.unshift(element)
+}
