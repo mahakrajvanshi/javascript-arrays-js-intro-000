8a12bb8677afe26caff11a951a542f867d4f066c
MAHAK RAJVANSHI <mahakrajvanshi@yahoo.com>
2017-12-28 01:14:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,8 @@ function destructivelyRemoveElementFromEndOfArray (array){
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array){
+  array = array.slice(0, array.length - 1)
+  return array
+}
