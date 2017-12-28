9ef99059af389f01d04ec2c565416bae48d6c627
MAHAK RAJVANSHI <mahakrajvanshi@yahoo.com>
2017-12-28 01:05:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,8 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
